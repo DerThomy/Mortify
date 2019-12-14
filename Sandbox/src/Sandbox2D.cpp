32b88746aeea7a073e01873e3b4c04730e9d1ace
Thomas Janis Köhler <t.koehler@student.tugraz.at>
2019-12-14 21:46:14 +0100

    Prittier API calls in Sandbox2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -40,9 +40,30 @@ void Sandbox2D::OnUpdate(Mortify::Timestep ts)
 	{
 		MT_PROFILE_SCOPE("Renderer Draw");
 		Mortify::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Mortify::Renderer2D::DrawQuad({{-1.0f, 0.0f, 0.0f }, { 0.8f, 0.8f }, 0.0f,  {0.8f, 0.2f, 0.3f, 1.0f}});
-		Mortify::Renderer2D::DrawQuad({{0.5f, -0.5f, 0.0f }, { 0.5f, 0.75f }, 0.0f,  {0.2f, 0.3f, 0.8f, 1.0f}});
-		Mortify::Renderer2D::DrawQuad({{ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, 0.0f,  glm::vec4(1.0f), m_CheckerboardTexture, 10.0f});
+		
+		Mortify::Renderer2D::DrawQuad({
+			{-1.0f, 0.0f, 0.0f },
+			{ 0.8f, 0.8f },
+			0.0f,
+			{0.8f, 0.2f, 0.3f, 1.0f}}
+		);
+		
+		Mortify::Renderer2D::DrawQuad({
+			{0.5f, -0.5f, 0.0f },
+			{ 0.5f, 0.75f },
+			1.0f,
+			{0.2f, 0.3f, 0.8f, 1.0f}}
+		);
+		
+		Mortify::Renderer2D::DrawQuad({
+			{ 0.0f, 0.0f, -0.1f },
+			{ 10.0f, 10.0f },
+			0.0f,
+			glm::vec4(1.0f),
+			m_CheckerboardTexture,
+			10.0f}
+		);
+		
 		Mortify::Renderer2D::EndScene();
 	}
 }
