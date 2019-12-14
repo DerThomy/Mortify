f7dc948a75acdfa8d44e82561497a1eb8385bd23
Thomas Janis Köhler <t.koehler@student.tugraz.at>
2019-12-14 02:10:13 +0100

    Added File checking in OpenGLShader ReadFile

--- Mortify/src/Platform/OpenGL/OpenGLShader.cpp
+++ Mortify/src/Platform/OpenGL/OpenGLShader.cpp
@@ -192,14 +192,22 @@ namespace Mortify
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], size);
+				in.close();
+			}
+			else
+			{
+				MT_CORE_ERROR("Could not read from file '{0}'", filePath);
+			}
 		}
 		else
 		{
-			MT_CORE_WARN("Could not open '{0}'", filePath);
+			MT_CORE_ERROR("Could not open '{0}'", filePath);
 		}
 
 		return result;
