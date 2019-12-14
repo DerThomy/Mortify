cb6b390a54b22386ffbe865d5f87c4e8515ca3ff
Thomas Janis Köhler <t.koehler@student.tugraz.at>
2019-12-14 17:36:01 +0100

    Moved Example Layer out to own file

---
+++ Sandbox/src/ExampleLayer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Mortify.h>
+
+class ExampleLayer : public Mortify::Layer
+{
+public:
+	ExampleLayer();
+
+	void OnUpdate(Mortify::Timestep ts) override;
+
+	virtual void OnImGuiRender() override;
+
+	void OnEvent(Mortify::Event& event) override;
+
+private:
+	Mortify::ShaderLibrary m_ShaderLibrary;
+	Mortify::Ref<Mortify::Shader> m_Shader;
+	Mortify::Ref<Mortify::VertexArray> m_VertexArray;
+
+	Mortify::Ref<Mortify::Shader> m_FlatColorShader;
+	Mortify::Ref<Mortify::VertexArray> m_SquareVA;
+
+	Mortify::Ref<Mortify::Texture2D> m_Texture, m_ChernoLogoTexture;
+
+	Mortify::OrthoCameraController m_CameraController;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
+};
