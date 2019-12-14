7b840b3f0b4177837d261cc50299269a5c3272a4
Thomas Janis Köhler <t.koehler@student.tugraz.at>
2019-12-14 17:41:32 +0100

    Added Layer iterators and changed premake architexture from x64 to correct x86_64

--- GenerateProject.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call vendor\bin\premake\premake5.exe vs2019
-
-IF %ERRORLEVEL% NEQ 0 (
-    PAUSE
-)
--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,5 @@
 workspace "Mortify"
-    architecture "x64"
+    architecture "x86_64"
     startproject "Sandbox"
 
     configurations { 
